622740f8748862665e46fd87043613c903ae3dc8
ireflux <mapleblue2007@gmail.com>
2022-01-03 17:52:49 +0800

    Mybatis 批量插入

---
+++ src/main/java/io/github/ireflux/westcitymall/config/MybatisPlusConfig.java
@@ -0,0 +1,31 @@
+package io.github.ireflux.westcitymall.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.core.incrementer.IKeyGenerator;
+import com.baomidou.mybatisplus.extension.incrementer.H2KeyGenerator;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public IKeyGenerator keyGenerator() {
+        return new H2KeyGenerator();
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+
+    @Bean
+    public BatchSqlInjector batchSqlInjector() {
+        return new BatchSqlInjector();
+    }
+
+}
